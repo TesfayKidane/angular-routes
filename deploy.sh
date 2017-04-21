fe581a284242d4b9dd1ff39a7e6525b5ff696478
Tesfay Aregay <Tesfay Aregay>
2017-04-21 14:45:02 -0500

    form update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ng build --prod --base-href="https://tesfaykidane.github.io/angular-routes/"
-git remote add origin https://github.com/TesfayKidane/angular-routes.git
 git add .
 git commit -m "form update"
 git push
