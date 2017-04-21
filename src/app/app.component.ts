59ce92f60a47faefbdbaf8216d5551c40a79fc32
Tesfay Aregay <Tesfay Aregay>
2017-04-21 14:47:20 -0500

    form update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
